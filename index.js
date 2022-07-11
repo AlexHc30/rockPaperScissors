da59cdce28894583388cd0b00bdc8366a8961b38
Alex Haiduc <haiduc.alx@gmail.com>
2022-07-11 20:57:47 +0300

    Modify playRound function to game()
    - Added for() loop to have 5 rounds
    - Implemented counter for player and coumputer wins
    - After the 5 rounds are finished gameOver() function is called
    - Depending on the final score, gameOver alerts an appropriate message
    - Simplified computerPlay()

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
+let computerOptions = ["rock", "paper", "scissors"];
+let playerSelection;
+window.onload = game();
+
 function computerPlay() {
-    let random = Math.floor(Math.random() * 3 + 1)
-    let computerSelection = "";
-    if (random === 1) {
-        computerSelection = "rock";
-    } else if (random === 2) {
-        computerSelection = "paper";
-    } else {
-        computerSelection = "scissors";
-    }
-    console.log(computerSelection)
-    return computerSelection;
+    return computerOptions[Math.floor(Math.random() * 3)];
 }
 
-const computerSelection = computerPlay();
-const playerSelection = prompt("Please choose between 'rock', 'paper' or 'scissors'.").toLowerCase();
-playRound(playerSelection, computerSelection);
 
-function playRound(playerSelection, computerSelection) {
-    console.log(playerSelection);
-    let roundResult = "";
-    let playerWon = (playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper');
-    console.log(playerWon)
-    if (playerSelection === computerSelection) {
-        roundResult = 'Draw! You both chose the same.'
-    } else if (playerWon) {
-        roundResult = `You win! ${playerSelection} beats ${computerSelection}`
-    } else {
-        roundResult = `You lose! ${computerSelection} beats ${playerSelection}`
+function game() {
+    let playerWins = 0;
+    let computerWins = 0;
+    for(let i = 0; i < 5; i++) {
+        computerSelection = computerPlay();
+        console.log(computerSelection);
+        playerSelection = prompt("Please choose between 'rock', 'paper' or 'scissors'.").toLowerCase();
+        console.log(playerSelection);
+        let roundResult = "";
+        let playerWon = (playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper');
+        console.log(playerWon);
+        if (playerSelection === computerSelection) {
+            roundResult = 'Draw! You both chose the same.'
+        } else if (playerWon) {
+            roundResult = `You win! ${playerSelection} beats ${computerSelection}`;
+            playerWins++;
+        } else {
+            roundResult = `You lose! ${computerSelection} beats ${playerSelection}`;
+            computerWins++;
+        }
+        alert(roundResult);
     }
-    alert(roundResult);
+    gameOver(playerWins, computerWins);
+}
+
+function gameOver(playerWins, computerWins) {
+    if (playerWins > computerWins) {
+        alert(`Game Over! Player has won the tournament ${playerWins} to ${computerWins}!`);
+    } else if (computerWins > playerWins) {
+        alert(`Game Over! Computer has won the tournament ${computerWins} to ${playerWins}!`);
+    } else {
+        alert(`Game Over! Player has won the tournament ${computerWins} to ${playerWins}!`);
+    }
 }
