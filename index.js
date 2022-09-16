31dcb8e83c3f46ea39d72e62c5ab6a30d755020e
Alex Haiduc <haiduc.alx@gmail.com>
2022-09-16 18:44:54 +0300

    Modify mechanics and add basic style > Added buttons for actions and created logic to replace alerts.
    Added on screen messages to display round result, current scores
    and final result.
    
    After the game is over, add button to restart game.

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
-let computerOptions = ["rock", "paper", "scissors"];
-let playerSelection;
-window.onload = game();
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', game))
+const options = ["rock", "paper", "scissors"]
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
-    return computerOptions[Math.floor(Math.random() * 3)];
+    return options[Math.floor(Math.random() * 3)];
 }
 
+function game(playerOption) {
+    let playerSelection = options[playerOption.target.className];
+    let computerSelection = computerPlay();
 
-function game() {
-    let playerWins = 0;
-    let computerWins = 0;
-    for(let i = 0; i < 5; i++) {
-        computerSelection = computerPlay();
-        console.log(computerSelection);
-        playerSelection = prompt("Please choose between 'rock', 'paper' or 'scissors'.").toLowerCase();
-        console.log(playerSelection);
-        let roundResult = "";
-        let playerWon = (playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper');
-        if (playerSelection === computerSelection) {
-            roundResult = 'Draw! You both chose the same.'
-        } else if (playerWon) {
-            roundResult = `You win! ${playerSelection} beats ${computerSelection}`;
-            playerWins++;
-        } else {
-            roundResult = `You lose! ${computerSelection} beats ${playerSelection}`;
-            computerWins++;
-        }
-        alert(roundResult);
-    }
-    gameOver(playerWins, computerWins);
-}
+    let roundResult = "";
+    const playerWon = (playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper');
 
-function gameOver(playerWins, computerWins) {
-    if (playerWins > computerWins) {
-        alert(`Game Over! Player has won the tournament ${playerWins} to ${computerWins}!`);
-    } else if (computerWins > playerWins) {
-        alert(`Game Over! Computer has won the tournament ${computerWins} to ${playerWins}!`);
+    if (playerSelection === computerSelection) {
+        roundResult = 'Draw! You both chose the same.'
+    } else if (playerWon) {
+        roundResult = `You win! ${playerSelection} beats ${computerSelection}`;
+        playerScore++;
     } else {
-        alert(`Game Over! Player has won the tournament ${computerWins} to ${playerWins}!`);
+        roundResult = `You lose! ${computerSelection} beats ${playerSelection}`;
+        computerScore++;
     }
+
+    const pScore = document.querySelector('.playerScore');
+    pScore.innerText = `Player score: ${playerScore}`;
+    const cScore = document.querySelector('.computerScore');
+    cScore.innerText = `Computer score: ${computerScore}`;
+    const roundMessage = document.querySelector('.roundMessage');
+    roundMessage.innerText = roundResult;
+
+    if(playerScore === 5 || computerScore === 5) {
+        buttons.forEach(button => button.removeEventListener('click', game))
+        gameOver(playerScore, computerScore);
+    }
+}
+
+function gameOver(playerScore, computerScore) {
+    if (playerScore > computerScore) {
+        alert(`Game Over! Player has won the tournament ${playerScore} to ${computerScore}!`);
+    } else if (computerScore > playerScore) {
+        alert(`Game Over! Computer has won the tournament ${computerScore} to ${playerScore}!`);
+    } else {
+        alert(`Game Over! Player has won the tournament ${computerScore} to ${playerScore}!`);
+    }
+
+    const restartButton = document.querySelector('.restart');
+    restartButton.style.display = 'block';
+    restartButton.onclick = () => location.reload()
 }
