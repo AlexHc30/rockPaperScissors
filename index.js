592d9d52ad35c1ef5bfb0a3cd54b2a945350adaa
Alex Haiduc <haiduc.alx@gmail.com>
2022-09-21 13:26:11 +0300

    Polish a few things Add logo image Created prompt to ask for player's name and added it in messages Added paragraph describing the rules Made some minor visual modifications

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
+const playerName = prompt('Please write your name');
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener('click', game))
-const options = ["rock", "paper", "scissors"]
+const weapons = ["rock", "paper", "scissors"]
 let playerScore = 0;
 let computerScore = 0;
 
 function computerPlay() {
-    return options[Math.floor(Math.random() * 3)];
+    return weapons[Math.floor(Math.random() * weapons.length)];
 }
 
 function game(playerOption) {
-    let playerSelection = options[playerOption.target.className];
+    let playerSelection = weapons[playerOption.target.value];
+    console.log(playerSelection)
+    console.log(playerOption.target.value)
     let computerSelection = computerPlay();
 
+    const pScore = document.querySelector('.playerScore');
+    // pScore.innerText = `${playerName} score: ${playerScore}`;
+    const cScore = document.querySelector('.computerScore');
+    // cScore.innerText = `Computer score: ${computerScore}`;
+    const roundMessage = document.querySelector('.roundMessage');
+
     let roundResult = "";
-    const playerWon = (playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') ||
+    const playerWon = (playerSelection === 'rock' && computerSelection === 'scissors') || 
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper');
 
     if (playerSelection === computerSelection) {
-        roundResult = 'Draw! You both chose the same.'
+        roundResult = `Draw! You both chose the ${playerSelection}.`
     } else if (playerWon) {
         roundResult = `You win! ${playerSelection} beats ${computerSelection}`;
         playerScore++;
@@ -25,13 +35,9 @@ function game(playerOption) {
         roundResult = `You lose! ${computerSelection} beats ${playerSelection}`;
         computerScore++;
     }
-
-    const pScore = document.querySelector('.playerScore');
-    pScore.innerText = `Player score: ${playerScore}`;
-    const cScore = document.querySelector('.computerScore');
-    cScore.innerText = `Computer score: ${computerScore}`;
-    const roundMessage = document.querySelector('.roundMessage');
     roundMessage.innerText = roundResult;
+    pScore.innerText = `${playerName} score: ${playerScore}`;
+    cScore.innerText = `Computer score: ${computerScore}`;
 
     if(playerScore === 5 || computerScore === 5) {
         buttons.forEach(button => button.removeEventListener('click', game))
@@ -43,7 +49,7 @@ function gameOver(playerScore, computerScore) {
     const finalResult = document.querySelector('.finalResult');
 
     if (playerScore > computerScore) {
-        finalResult.innerText = `Game Over! Player has won the tournament ${playerScore} to ${computerScore}!`;
+        finalResult.innerText = `Game Over! ${playerName} has won the tournament ${playerScore} to ${computerScore}!`;
     } else {
         finalResult.innerText = `Game Over! Computer has won the tournament ${computerScore} to ${playerScore}!`;
     }
